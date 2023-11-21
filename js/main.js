bf7d72e54f741de51fd86c2e6ac03a6017b4d2d9
Bhuvnish <b_arora163217@fanshaweonline.ca>
2023-11-21 12:46:23 -0500

    spinner

--- js/main.js
+++ js/main.js
@@ -5,6 +5,41 @@
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
 
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const spinnerContainer = document.getElementById("spinner-container");
+  
+    const spinner = `<svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+      viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+      <path fill="#333" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+      <animateTransform
+      attributeName="transform"
+      attributeType="XML"
+      type="rotate"
+      dur="1s"
+      from="0 50 50"
+      to="360 50 50"
+      repeatCount="indefinite" />
+      </path>
+      </svg>`;
+  
+    spinnerContainer.innerHTML = spinner;
+  
+    window.addEventListener("load", function () {
+      // Remove the spinner once the page has fully loaded
+      spinnerContainer.innerHTML = "";
+    });
+  });
+  
+
+
+
+
+
+  
+
   function modelLoaded() {
     hotspots.forEach(hotspot => {
       hotspot.style.display = "block";
@@ -107,7 +142,7 @@ function loadInfoBoxes() {
           materialList.appendChild(clone);
         });
       })
-      .catch(error => console.error(error));
+      .catch(error => console.error(error));//catch and report any errors
   }
 
   function showInfo() {
